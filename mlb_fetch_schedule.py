29e4d02e3f4e0de56c8449f038ccf87e8629df05
Jen Ting Huang <jentinghuang@Jens-MacBook-Pro.local>
2024-08-04 14:18:29 -0400

    Remove unnecessary conditional statement

--- mlb_fetch_schedule.py
+++ mlb_fetch_schedule.py
@@ -3,10 +3,13 @@ from datetime import datetime, timedelta, timezone
 import pytz
 import json
 from config import API_KEY, SPORT
+from s3_uploader import upload_to_s3, delete_from_s3
+
 
 class MLBAPI:
 
     BASE_URL = 'https://statsapi.mlb.com/api/v1/'
+    BUCKET_NAME = 'timjimmymlbdata'
     
     @staticmethod
     def get_schedule(sport_id = 1):
@@ -29,7 +32,7 @@ class MLBAPI:
         return response.json()
     
     @staticmethod
-    def save_schedule(data, filename="./mlb_schedule.json"):
+    def save_schedule(data, filename="/tmp/mlb_schedule.json"):
         """
         Save the given data to a JSON file.
 
@@ -135,8 +138,7 @@ class MLBAPI:
     
     def get_utc_start_and_end():
         """
-        Get the UTC start and end times for the current day. If the current time is between midnight 
-        and 3 AM UTC, adjust the range to cover the previous day.
+        Get the start and end times for today in UTC.
 
         Returns:
             tuple: A tuple containing the start and end times in ISO 8601 format.
@@ -144,22 +146,14 @@ class MLBAPI:
         # Get the current UTC time
         now_utc = datetime.now(timezone.utc)
 
-        # If the current time is between midnight and 3 AM UTC, adjust to the previous day
-        if 0 <= now_utc.hour < 3:
-            # Set the start of the day to the previous day
-            start_of_today_utc = now_utc - timedelta(days=1)
-        else:
-            # Otherwise, set the start of the day to today
-            start_of_today_utc = now_utc
-
         # Reset the time to midnight of the start day
-        start_of_today_utc = start_of_today_utc.replace(hour=0, minute=0, second=0, microsecond=0)
+        now_utc = now_utc.replace(hour=0, minute=0, second=0, microsecond=0)
         
         # Set the end time to 2 AM of the next day
-        end_of_today_utc = start_of_today_utc + timedelta(days=1, hours=2)
+        end_of_today_utc = now_utc + timedelta(days=1, hours=2)
 
         # Convert the times to ISO 8601 format
-        today_str = start_of_today_utc.strftime('%Y-%m-%dT%H:%M:%SZ')
+        today_str = now_utc.strftime('%Y-%m-%dT%H:%M:%SZ')
         end_str = end_of_today_utc.strftime('%Y-%m-%dT%H:%M:%SZ')
 
         return today_str, end_str
@@ -167,9 +161,12 @@ class MLBAPI:
 
 def main():
     try:
+        # delete old schedule
+        delete_from_s3(MLBAPI.BUCKET_NAME, "mlb_schedule.json")
         schedule_data = MLBAPI.get_schedule()
         games = MLBAPI.process_game_data(schedule_data)
         MLBAPI.save_schedule(games)
+        upload_to_s3("/tmp/mlb_schedule.json", MLBAPI.BUCKET_NAME, "mlb_schedule.json")
         print("Schedule fetched and saved successfully.")
     except Exception as e:
         print(f"An error occurred: {str(e)}")
